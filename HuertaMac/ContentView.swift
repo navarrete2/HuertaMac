443905bbfec684bdadaf46e530638bd5e9429d48
navarrete2 <m4gu4l@icloud.com>
2024-03-12 20:37:39 +0100

    para otro proyecto

--- HuertaMac/ContentView.swift
+++ HuertaMac/ContentView.swift
@@ -4,39 +4,39 @@
 //
 //  Created by Miguel  on 10/3/24.
 //import SwiftUI
-
 import SwiftUI
 import SDWebImageSwiftUI
-import FirebaseAppCheck
 import FirebaseAnalytics
-
+import FirebaseAppCheck
 struct ContentView: View {
     @ObservedObject var productoViewModel = ProductoViewModel()
 
     var body: some View {
-        VStack {
+        NavigationView {
             List(productoViewModel.productos) { producto in
-                VStack(alignment: .leading) {
-                    Text(producto.nombre).font(.headline)
-                    Text(producto.descripcion)
-                    Text("\(producto.precio)")
-                    
-                    // Aquí es donde se muestra la imagen.
-                    if let url = URL(string: producto.imagen) {
-                        WebImage(url: url)
-                            .resizable()
-                            .aspectRatio(contentMode: .fill)
-                    } else {
-                        // Maneja el caso en que producto.imagen no es una URL válida.
-                        // Por ejemplo, podrías mostrar una imagen predeterminada.
+                NavigationLink(destination: DetalleProductoView(producto: producto)) {
+                    VStack(alignment: .leading) {
+                        Text(producto.nombre).font(.headline)
+                        Text(producto.descripcion)
+                        Text("\(producto.precio)")
+                        
+                        // Aquí es donde se muestra la imagen.
+                        if let url = URL(string: producto.imagen) {
+                            WebImage(url: url)
+                                .resizable()
+                                .aspectRatio(contentMode: .fill)
+                        } else {
+                            // Maneja el caso en que producto.imagen no es una URL válida.
+                            // Por ejemplo, podrías mostrar una imagen predeterminada.
+                        }
                     }
-                }
-                .onAppear {
-                    Analytics.logEvent("producto_visto", parameters: [
-                        "nombre": producto.nombre,
-                        "descripcion": producto.descripcion,
-                        "precio": "\(producto.precio)"
-                    ])
+                    .onAppear {
+                        Analytics.logEvent("producto_visto", parameters: [
+                            "nombre": producto.nombre,
+                            "descripcion": producto.descripcion,
+                            "precio": "\(producto.precio)"
+                        ])
+                    }
                 }
             }
             .onAppear() {
